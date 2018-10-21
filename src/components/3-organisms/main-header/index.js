ba8ed21bd68e23d052e0e92a8c8d7fa116f30819
daengdaengLee <gunho1020@gmail.com>
2018-10-21 13:25:07 +0900

    ENH: Make main-header comp

---
+++ src/components/3-organisms/main-header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MainHeader = () => (
+  <div
+    style={{
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontWeight: 600,
+    }}
+  >
+    CSV to Line Chart
+  </div>
+);
+
+export default MainHeader;
