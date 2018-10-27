c1841744fd541ea1ba60b7fb44bd3075bae10173
daengdaengLee <gunho1020@gmail.com>
2018-10-28 02:45:23 +0900

    ENH: Make utility functions
    - _pipe
    - _go
    - _singleValueToArray

---
+++ src/assets/js/utils.js
@@ -0,0 +1,14 @@
+import _ from 'underscore';
+
+export function _pipe(funcs) {
+  const reversedFuncs = [...funcs].reverse();
+  return _.compose(...reversedFuncs);
+}
+
+export function _go(val, ...funcs) {
+  return _pipe(...funcs)(val);
+}
+
+export function _singleValueToArray(val) {
+  return _.isArray(val) ? val : [val];
+}
