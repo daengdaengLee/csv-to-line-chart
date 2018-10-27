c54cb94d8cb94f1d8a199fee355cac7298a179ab
daengdaengLee <gunho1020@gmail.com>
2018-10-28 04:38:01 +0900

    ENH: Make saga for uploadCsvFile action in csvs reducer

--- src/ducks/sagas/csvs.js
+++ src/ducks/sagas/csvs.js
@@ -1,8 +1,10 @@
-import { all, take, call, put } from 'redux-saga/effects';
+import { all, take, call, put, select } from 'redux-saga/effects';
 import _ from 'underscore';
-import { INIT, setAllIds } from '../modules/csvs';
+import { _pipe } from '../../assets/js/utils';
+import { INIT, setAllIds, UPLOAD_CSV_FILE, addIds } from '../modules/csvs';
 import localforage from '../../localforage';
 
+window.localforage = localforage;
 // Workers
 function* getCsvsFromLocalforage() {
   let csvs;
@@ -21,6 +23,45 @@ function* getCsvsFromLocalforage() {
   return csvs;
 }
 
+function* updateCsvsToLocalforage(fileName) {
+  let success;
+  const oldCsvs = yield select(_pipe(_.property('csvs'), _.property('allIds')));
+  const newCsvs = [...oldCsvs, fileName];
+  try {
+    yield call(_.bind(localforage.setItem, localforage, 'csvs', newCsvs));
+    success = true;
+  } catch (error) {
+    console.error(error);
+    success = false;
+  }
+  return success;
+}
+
+function getStringFromCsvFile(file) {
+  if (!window.FileReader) return;
+  return new Promise((res, rej) => {
+    const reader = new FileReader();
+    reader.onload = evt => {
+      if (evt.target.readyState !== 2) rej();
+      return res(evt.target.result);
+    };
+    reader.onerror = () => rej();
+    reader.readAsText(file);
+  });
+}
+
+function* setCsvToLocalforage(name, csvAsString) {
+  let success;
+  try {
+    yield call(_.bind(localforage.setItem, localforage, name, csvAsString));
+    success = true;
+  } catch (error) {
+    console.error(error);
+    success = false;
+  }
+  return success;
+}
+
 // Watchers
 function* watchInit() {
   while (true) {
@@ -30,6 +71,17 @@ function* watchInit() {
   }
 }
 
+function* watchUploadCsvFile() {
+  while (true) {
+    const action = yield take(UPLOAD_CSV_FILE);
+    const { name: fileName } = action.file;
+    const csvAsString = yield call(getStringFromCsvFile, action.file);
+    let success = yield call(setCsvToLocalforage, fileName, csvAsString);
+    success = yield success && call(updateCsvsToLocalforage, fileName);
+    yield success && put(addIds(fileName));
+  }
+}
+
 export default function* csvsSaga() {
-  yield all([watchInit()]);
+  yield all([watchInit(), watchUploadCsvFile()]);
 }
