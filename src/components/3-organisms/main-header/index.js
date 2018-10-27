6493008d8151458f6e8750b9a0f91442569083b6
daengdaengLee <gunho1020@gmail.com>
2018-10-28 00:18:04 +0900

    ENH: Make refresh link in main header's title

--- src/components/3-organisms/main-header/index.js
+++ src/components/3-organisms/main-header/index.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MainHeader = () => (
-  <div
-    style={{
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontWeight: 600,
-    }}
-  >
-    CSV to Line Chart
-  </div>
-);
+const STYLE = {
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontWeight: 600,
+  cursor: 'pointer',
+};
+
+class MainHeader extends Component {
+  render() {
+    const { _onClickTitle } = this;
+    return (
+      <h1 style={STYLE} onClick={_onClickTitle}>
+        CSV to Line Chart
+      </h1>
+    );
+  }
+
+  _onClickTitle() {
+    window.location.reload(true);
+  }
+}
 
 export default MainHeader;
