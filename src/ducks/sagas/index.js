2c6a41ba6f88c63985774fd8176a853c0cdfbdb6
daengdaengLee <gunho1020@gmail.com>
2018-10-28 11:18:38 +0900

    ENH: Register widgets saga to root saga

--- src/ducks/sagas/index.js
+++ src/ducks/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
+import widgetsSaga from './widgets';
 import csvsSaga from './csvs';
 
 export default function* rootSaga() {
-  yield all([csvsSaga()]);
+  yield all([widgetsSaga(), csvsSaga()]);
 }
