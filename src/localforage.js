e9ccf243534007cd48f66eb959eb8079d81e5710
daengdaengLee <gunho1020@gmail.com>
2018-10-28 22:48:57 +0900

    TST: Register localforage to global object when dev env

--- src/localforage.js
+++ src/localforage.js
@@ -2,4 +2,8 @@ import localforage from 'localforage';
 
 localforage.config({ name: 'csv-to-line-chart' });
 
+if (process.env.NODE_ENV === 'production') {
+  window.localforage = localforage;
+}
+
 export default localforage;
