1e0e3649e20e7bb857632ea25b6bc8632389ca62
daengdaengLee <gunho1020@gmail.com>
2018-10-28 11:59:25 +0900

    ENH: Add more fallback code at interface of local forage

--- src/ducks/sagas/csvs.js
+++ src/ducks/sagas/csvs.js
@@ -32,65 +32,111 @@ function* getCsvsFromLocalforage() {
 
 function* addCsvsToLocalforage(keys) {
   let success;
-  const oldCsvs = yield select(_pipe(_.property('csvs'), _.property('allIds')));
-  const newCsvs = yield call(_.bind(Array.prototype.concat, oldCsvs, keys));
-  try {
-    yield call(_.bind(localforage.setItem, localforage, 'csvs', newCsvs));
-    success = true;
-  } catch (error) {
-    console.error(error);
-    success = false;
+  let ok = false;
+  let count = 0;
+  while (!ok) {
+    try {
+      const oldCsvs = yield select(
+        _pipe(_.property('csvs'), _.property('allIds')),
+      );
+      const newCsvs = yield call(_.bind(Array.prototype.concat, oldCsvs, keys));
+      yield call(_.bind(localforage.setItem, localforage, 'csvs', newCsvs));
+      success = true;
+      ok = true;
+    } catch (error) {
+      console.error(error);
+      success = false;
+      count++;
+      ok = count > 2;
+    }
   }
   return success;
 }
 
 function* removeCsvsFromLocalforage(keys) {
   let success;
-  const oldCsvs = yield select(_pipe(_.property('csvs'), _.property('allIds')));
-  const newCsvs = yield call(_.difference, oldCsvs, _singleValueToArray(keys));
-  try {
-    yield call(_.bind(localforage.setItem, localforage, 'csvs', newCsvs));
-    success = true;
-  } catch (error) {
-    console.error(error);
-    success = false;
+  let ok = false;
+  let count = 0;
+  while (!ok) {
+    try {
+      const oldCsvs = yield select(
+        _pipe(_.property('csvs'), _.property('allIds')),
+      );
+      const newCsvs = yield call(
+        _.difference,
+        oldCsvs,
+        _singleValueToArray(keys),
+      );
+      yield call(_.bind(localforage.setItem, localforage, 'csvs', newCsvs));
+      success = true;
+      ok = true;
+    } catch (error) {
+      console.error(error);
+      success = false;
+      count++;
+      ok = count > 2;
+    }
   }
   return success;
 }
 
 function getStringFromCsvFile(key) {
-  if (!window.FileReader) return;
+  if (!window.FileReader)
+    return new Promise((_, rej) => {
+      console.error('no FileReader API');
+      rej(undefined);
+    });
   return new Promise((res, rej) => {
     const reader = new FileReader();
     reader.onload = evt => {
-      if (evt.target.readyState !== 2) rej(undefined);
+      if (evt.target.readyState !== 2) {
+        console.error('FileReader state is not ok');
+        rej(undefined);
+      }
       return res(evt.target.result);
     };
-    reader.onerror = () => rej(undefined);
+    reader.onerror = error => {
+      console.error(error);
+      rej(undefined);
+    };
     reader.readAsText(key);
   });
 }
 
 function* setCsvToLocalforage(key, csvAsString) {
   let success;
-  try {
-    yield call(_.bind(localforage.setItem, localforage, key, csvAsString));
-    success = true;
-  } catch (error) {
-    console.error(error);
-    success = false;
+  let ok = false;
+  let count = 0;
+  while (!ok) {
+    try {
+      yield call(_.bind(localforage.setItem, localforage, key, csvAsString));
+      success = true;
+      ok = true;
+    } catch (error) {
+      console.error(error);
+      success = false;
+      count++;
+      ok = count > 2;
+    }
   }
   return success;
 }
 
 function* unsetCsvFromLocalforage(key) {
   let success;
-  try {
-    yield call(_.bind(localforage.removeItem, localforage, key));
-    success = true;
-  } catch (error) {
-    console.error(error);
-    success = false;
+  let ok = false;
+  let count = 0;
+  while (!ok) {
+    try {
+      yield call(_.bind(localforage.removeItem, localforage, key));
+      success = true;
+      ok = true;
+    } catch (error) {
+      console.error(error);
+      success = false;
+      count++;
+      ok = count > 2;
+    }
   }
   return success;
 }
